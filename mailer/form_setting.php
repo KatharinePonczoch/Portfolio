f6c1fe2993a26a0301c805407b7b7b1ea55fa35b
Katharine <katharine.ponczoch@gmail.com>
2023-03-23 21:05:12 -0600

    initial commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Katharine Ponczoch'"; //letter subject
+	$to = 'katharine.ponczoch@gmail.com'; // Enter Your E-mail
+	$from = 'admin@portfoliowebsite.com'; // Admin e-mail
+	$fromName = 'Portfolio Website'; // Your company name
+	$charset = 'UTF-8';
+?>
